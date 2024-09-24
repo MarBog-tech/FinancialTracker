2b5a432e476196eafb32597771cafef11a96b226
DESKTOP-DBEI4M5\MarBog <marchuk5982@gmail.com>
2024-09-24 21:31:02 +0300

    added migrations

---
+++ FinancialTracker.Server/Migration/20240822211558_user_profile_update.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancialTracker.Server.Migration
+{
+    /// <inheritdoc />
+    public partial class user_profile_update : Microsoft.EntityFrameworkCore.Migrations.Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_UserProfile_UserProfileId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_UserProfileId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserProfileId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "SecondName",
+                table: "UserProfile",
+                newName: "Surname");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "UserProfile",
+                newName: "Name");
+
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JwtTokenId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Refresh_Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsValid = table.Column<bool>(type: "bit", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProfile_AspNetUsers_Id",
+                table: "UserProfile",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProfile_AspNetUsers_Id",
+                table: "UserProfile");
+
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "UserProfile",
+                newName: "SecondName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "UserProfile",
+                newName: "FirstName");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AccountId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserProfileId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_UserProfileId",
+                table: "AspNetUsers",
+                column: "UserProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_UserProfile_UserProfileId",
+                table: "AspNetUsers",
+                column: "UserProfileId",
+                principalTable: "UserProfile",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
